a1f90f63d13f0c4f432a2bc805a0b03593ec8bc4
sunyifeng <9339996@qq.com>
2018-04-01 16:29:34 +0800

    create project

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @program: data-structures
+ * @description: test
+ * @author: Mr.Sun
+ * @create: 2018-04-01 16:25
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
